35444e26004eace32b23c2bd48a249e6627f38a0
s8sg <swarvanusg@gmail.com>
2018-11-02 11:59:04 +0900

    Update minio statemanager with updated definiton
    Signed-off-by: s8sg <swarvanusg@gmail.com>

--- minio.go
+++ minio.go
@@ -1,46 +1,109 @@
 package faasflowMinioStateManager
 
 import (
+	"bytes"
 	"fmt"
 	minio "github.com/minio/minio-go"
 	faasflow "github.com/s8sg/faasflow"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
 )
 
 type MinioStateManager struct {
-	initialized bool
+	bucketName  string
+	flowName    string
 	minioClient *minio.Client
 }
 
-func GetMinioStateManager() (*MinioStateManager, error) {
+// GetMinioStateManager Initialize a minio StateManager object based on configuration
+// Depends on s3_url, s3-secret-key, s3-access-key, [s3_bucket, s3_region](optional), workflow_name
+func GetMinioStateManager() (faasflow.StateManager, error) {
 
 	minioStateManager := &MinioStateManager{}
 
 	region := regionName()
 	bucketName := bucketName()
+	flowName := flowName()
+	if len(flowName) == 0 {
+		return nil, fmt.Errorf("Failed to initialize minio, workflow name must be specified")
+	}
 
 	minioClient, connectErr := connectToMinio(region)
 	if connectErr != nil {
 		return nil, fmt.Errorf("Failed to initialize minio, error %s", connectErr.Error())
 	}
 
-	minioStateManager.initialized = true
+	minioClient.MakeBucket(bucketName, region)
+
+	minioStateManager.bucketName = bucketName
+	minioStateManager.flowName = flowName
 
 	return minioStateManager, nil
 }
 
-func (minioState *MinioStateManager) Set(key string, value interface{}) error {
+func (minioState *MinioStateManager) Set(key string, value string) error {
+	if minioState.minioClient == nil {
+		return fmt.Errorf("minio client not initialized, use GetMinioStateManager()")
+	}
 
+	fullPath := getPath(minioState.bucketName, minioState.flowName, key)
+	reader := bytes.NewReader([]byte(value))
+	n, err := minioState.minioClient.PutObject(minioState.bucketName,
+		fullPath,
+		reader,
+		int64(reader.Len()),
+		minio.PutObjectOptions{})
+	if err != nil {
+		return fmt.Errorf("error writing: %s, error: %s", fullPath, err.Error())
+	}
 	return nil
 }
 
-func (minioState *MinioStateManager) Get(key string) (interface{}, error) {
+func (minioState *MinioStateManager) Get(key string) (string, error) {
+	if minioState.minioClient == nil {
+		return "", fmt.Errorf("minio client not initialized, use GetMinioStateManager()")
+	}
 
-	return nil
+	fullPath := getPath(minioState.bucketName, minioState.flowName, key)
+	obj, err := minioState.minioClient.GetObject(minioState.bucketName, fullPath, minio.GetObjectOptions{})
+	if err != nil {
+		return "", fmt.Errorf("error reading: %s, error: %s", fullPath, err.Error())
+	}
+
+	data, _ := ioutil.ReadAll(obj)
+
+	return string(data), nil
 }
 
 func (minioState *MinioStateManager) Del(key string) error {
+	if minioState.minioClient == nil {
+		return fmt.Errorf("minio client not initialized, use GetMinioStateManager()")
+	}
 
+	fullPath := getPath(minioState.bucketName, minioState.flowName, key)
+	err := minioState.minioClient.RemoveObject(minioState.bucketName, fullPath)
+	if err != nil {
+		return fmt.Errorf("error removing: %s, error: %s", fullPath, err.Error())
+	}
 	return nil
+}
+
+func readSecret(key string) (string, error) {
+	basePath := "/var/openfaas/secrets/"
+	if len(os.Getenv("secret_mount_path")) > 0 {
+		basePath = os.Getenv("secret_mount_path")
+	}
+
+	readPath := path.Join(basePath, key)
+	secretBytes, readErr := ioutil.ReadFile(readPath)
+	if readErr != nil {
+		return "", fmt.Errorf("unable to read secret: %s, error: %s", readPath, readErr)
+	}
+	val := strings.TrimSpace(string(secretBytes))
+	return val, nil
 }
 
 func connectToMinio(region string) (*minio.Client, error) {
@@ -49,16 +112,16 @@ func connectToMinio(region string) (*minio.Client, error) {
 
 	tlsEnabled := tlsEnabled()
 
-	secretKey, _ := sdk.ReadSecret("s3-secret-key")
-	accessKey, _ := sdk.ReadSecret("s3-access-key")
+	secretKey, _ := readSecret("s3-secret-key")
+	accessKey, _ := readSecret("s3-access-key")
 
 	return minio.New(endpoint, accessKey, secretKey, tlsEnabled)
 }
 
 // getPath produces a string such as pipeline/
-func getPath(bucket string, p *sdk.PipelineLog) string {
-	fileName := "faasflow.state"
-	return fmt.Sprintf("%s/%s", bucket, fileName)
+func getPath(bucket string, flowName string, key string) string {
+	fileName := fmt.Sprintf("%s.value", key)
+	return fmt.Sprintf("%s/%s/%s", bucket, flowName, fileName)
 }
 
 func tlsEnabled() bool {
@@ -75,6 +138,14 @@ func bucketName() string {
 		log.Printf("Bucket name not found, set to default: %v\n", bucketName)
 	}
 	return bucketName
+}
+
+func flowName() string {
+	flowName, exist := os.LookupEnv("workflow_name")
+	if exist == false || len(flowName) == 0 {
+		flowName = ""
+	}
+	return flowName
 }
 
 func regionName() string {
